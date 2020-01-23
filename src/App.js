97f989d59ec93c5857234adfbf9fe17d7309bf63
ziv24 <ziv2471993@gmail.com>
2020-01-23 13:31:43 +0200

    build form done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import StartForm from './components/StartForm';
+import { Container, Button } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      starterForm: false
+    }
+  }
+
+  onStart = e => {
+    this.setState({ starterForm: !this.state.starterForm });
+  }
+
+  render() {
+    const { starterForm } = this.state;
+    return (
+      <div className="App">
+        <Container>
+          <h1 style={{ margin: "50px" }}>Welcome To The Last Exam Game!</h1>
+          {starterForm ? <StartForm /> : <Button variant="dark" onClick={this.onStart}>START</Button>}
+        </Container>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/StartForm.jsx
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import { Row, Form, Col, Button, Container } from "react-bootstrap";
+
+export default class StartForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      range: [],
+      player_name: "",
+      range_from: "",
+      range_to: "",
+      player_number: 0
+    };
+  }
+
+  makeRangeArr = () => {
+    var list = [];
+    for (var i = this.state.range_from; i <= this.state.range_to; i++) {
+      list.push(i);
+    }
+    console.log(list);
+    this.setState({ range: list });
+  };
+  onPlayClick = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  onChangeName = e => {
+    this.setState({ player_name: e.target.value });
+  };
+
+  onLiveTo = () => {
+    if (this.state.range_to <= this.state.range_from) {
+      alert("Plaese enter number higher of the from number");
+      this.setState({ range_to: "" });
+    } else {
+      this.makeRangeArr();
+    }
+  };
+
+  onChangeNumber = e => {
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const {
+      range,
+      player_name,
+      range_from,
+      range_to,
+      player_number
+    } = this.state;
+    return (
+      <Container>
+        <Form style={{ width: "60%", margin: "auto" }}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              onChange={this.onChangeName}
+              type="text"
+              placeholder="Enter name"
+              value={player_name}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Number Range</Form.Label>
+
+            <Form.Control
+              type="number"
+              placeholder="from"
+              name="range_from"
+              value={range_from}
+              onChange={this.onChangeNumber}
+              required
+            />
+            <Form.Control
+              type="number"
+              placeholder="to"
+              name="range_to"
+              value={range_to}
+              onChange={this.onChangeNumber}
+              onBlur={this.onLiveTo}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group controlId="exampleForm.ControlSelect1">
+            <Form.Label>Select Number</Form.Label>
+            <Form.Control
+              as="select"
+              onChange={this.onChangeNumber}
+              name="player_number"
+              required
+            >
+              {range.map(num => (
+                <option key={num}>{num}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+
+          <Button variant="dark" onClick={this.onPlayClick}>
+            PLAY
+          </Button>
+        </Form>
+      </Container>
+    );
+  }
+}
